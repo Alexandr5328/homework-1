ca9e9f46390fc44f5c0c4713b2f437011b551939
Alexandr5328 <152407128+Alexandr5328@users.noreply.github.com>
2023-12-02 23:26:22 +0300

    Add files via upload

---
+++ дз-1ч.py
@@ -0,0 +1,3 @@
+x = 5.2356346
+rounded_num = round(x, 3)
+print('Округленное_число:', rounded_num)
---
+++ дз-2ч.py
@@ -0,0 +1,5 @@
+my_list = [33,8]
+average = sum(my_list) / len(my_list)
+x = average
+rounded_num = round(x)
+print('Среднее арифметическое:', rounded_num)
